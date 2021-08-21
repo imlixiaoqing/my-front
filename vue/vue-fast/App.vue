bb5c0991c6656fcf7060ceb58e7469b75111817d
李小庆 <lixiaoqing0126@outlook.com>
2021-08-21 17:00:24 +0800

    笔记

--- vue/vue-fast/App.vue
+++ vue/vue-fast/App.vue
@@ -1,3 +1,31 @@
 <template>
-  <h1>Hello!</h1>
+  <div>
+    <p>name: {{ person.name }}</p>
+    <p>name: {{ person.age }}</p>
+    <p>sex: {{ person.sex }}</p>
+    <p>arr: {{ person.arr[0] }}</p>
+    <button @click="handle">test</button>
+  </div>
 </template>
+
+<script lang='ts'>
+export default {
+  data() {
+    return {
+      person: {
+        name: 'lxq',
+        age: 500,
+        arr: ['arr1', 'arr2']
+      }
+    }
+  },
+  methods: {
+    handle() {
+    }
+  },
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
